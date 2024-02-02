5d6ab2d9164e0d48efd525a6f8d8eeddc6337279
Neebooo <imnano@mailfence.com>
2024-02-02 12:51:43 +0100

    Push Privacy

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,105 @@
+<main class="p-6">
+	<div class="max-w-2xl mx-auto bg-white p-8 rounded-md shadow-md text-center flex flex-col gap-4">
+		<h1 class="text-2xl font-bold mb-6">OnlyMySpace - Privacy Policy</h1>
+
+		<p class="mb-6">Last Updated: 2024-02-02</p>
+
+		<section class="mb-8">
+			<h2 class="text-xl font-bold mb-2">1. Introduction</h2>
+			<p>
+				Welcome to OnlyMySpace! This Privacy Policy outlines how we collect, use, and protect your
+				personal information when you use our site.
+			</p>
+		</section>
+
+		<section class="mb-8">
+			<h2 class="text-xl font-bold mb-2">2. Information We Collect</h2>
+			<p>
+				<b>Personal Information</b>: We may collect personal information such as name, email
+				address, and other contact details when users voluntarily provide them.
+			</p>
+			<p>
+				<b>Usage Information</b>: We collect information about how users interact with our site,
+				including IP addresses, browser types, and pages visited.
+			</p>
+		</section>
+		<section class="mb-8">
+			<h2 class="text-xl font-bold mb-2">3. Use of Information</h2>
+			<p>We may use the information we collect for the following purposes:</p>
+			<div class="ml-4">
+				<ul class="list-disc">
+					<li class="mb-2 list-item">To provide and improve our services</li>
+					<li class="mb-2 list-item">To communicate with users</li>
+					<li class="mb-2 list-item">To personalize user experience</li>
+				</ul>
+			</div>
+		</section>
+
+		<section class="mb-8">
+			<h2 class="text-xl font-bold mb-2">4. Protection of Information</h2>
+			<p>We take appropriate measures to protect user information:</p>
+			<div class="ml-4">
+				<ul class="list-disc">
+					<li class="mb-2 list-item">
+						We use industry-standard security measures to safeguard user data.
+					</li>
+					<li class="mb-2 list-item">
+						Access to user information is restricted to authorized personnel only.
+					</li>
+				</ul>
+			</div>
+		</section>
+
+		<section class="mb-8">
+			<h2 class="text-xl font-bold mb-2">5. Sharing of Information</h2>
+			<p>
+				We do not sell, trade, or otherwise transfer user information to outside parties unless we
+				provide users with advance notice.
+			</p>
+		</section>
+
+		<section class="mb-8">
+			<h2 class="text-xl font-bold mb-2">6. Third-Party Links</h2>
+			<p>
+				Our site may contain links to third-party websites. We are not responsible for the privacy
+				practices or content of these sites.
+			</p>
+		</section>
+
+		<section class="mb-8">
+			<h2 class="text-xl font-bold mb-2">7. Cookies</h2>
+			<p>
+				We may use cookies to enhance user experience. Users can choose to set their web browser to
+				refuse cookies or to alert them when cookies are being sent.
+			</p>
+		</section>
+		<section class="mb-8">
+			<h2 class="text-xl font-bold mb-2">
+				8. Children's Online Privacy Protection Act (COPPA) Compliance
+			</h2>
+			<p>
+				Our site is not directed at children under the age of 13. We do not knowingly collect
+				personal information from children.
+			</p>
+		</section>
+		<section class="mb-8">
+			<h2 class="text-xl font-bold mb-2">9. Changes to this Privacy Policy</h2>
+			<p>
+				We reserve the right to update or modify this Privacy Policy at any time. Users will be
+				notified of any changes via email or prominent notice on our site.
+			</p>
+		</section>
+
+        <section>
+            <h2 class="text-xl font-bold mb-2">10. Your Consent</h2>
+            <p>By using our site, you consent to our Privacy Policy.</p>
+        </section>
+
+        <section>
+            <h2 class="text-xl font-bold mb-2">11. Contact Us</h2>
+            <p>If you have any questions or concerns about our Privacy Policy, please contact us at <a class="link link-primary" href="mailto:support@only-my.space">support@only-my.space</a>.</p>
+        </section>
+
+		<p class="text-sm text-gray-500">Thank you for being a part of OnlyMySpace!</p>
+	</div>
+</main>
