15633c7e422ae07d3b4c2cd002605024d6a34cb0
Neebooo <imnano@mailfence.com>
2024-02-27 15:45:55 +0100

    Added favicon and badges

--- src/components/profilesubcomp/UserCard.svelte
+++ src/components/profilesubcomp/UserCard.svelte
@@ -3,6 +3,14 @@
 
     export let profile: UserProfile;
 	export let badges: string[] = [];
+
+	let badgeMap: { [key: string]: string } = {
+		"OG": "/badges/OG.png",
+		"ADMIN": "/badges/ADMIN.png",
+		"DONATOR": "/badges/DONATOR.png",
+		"VERIFIED": "/badges/VERIFIED.png"
+	};
+
 </script>
 
 <img
@@ -18,6 +26,8 @@
 </h1>
 <div class="flex flex-wrap justify-center gap-2">
 	{#each badges as badge}
-	<span class="badge badge-lg badge-outline badge-primary">{badge}</span>
+	<span class="badge badge-lg badge-primary tooltip" data-tip={badge}>
+		<img src={badgeMap[badge]} alt={badge} class="w-6 h-6" class:w-8={badge == "OG"} />
+	</span>
 	{/each}
 </div>
