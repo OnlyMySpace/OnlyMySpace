131528a93cbb14828843f9f8593e64efaecae1da
Neebooo <imnano@mailfence.com>
2024-03-09 19:04:25 +0100

    Fix auth

--- generateJWT.cjs
+++ generateJWT.cjs
@@ -3,9 +3,9 @@
 const jose = require('jose');
 // Make a jwt
 async function a(){
-    const secret = new TextEncoder().encode();
+    const secret = new TextEncoder().encode("***REMOVED***");
     const alg = 'HS256';
-    const jwt = await new jose.SignJWT({ email:"" }).setProtectedHeader({ alg }).setExpirationTime('1d').sign(secret);
+    const jwt = await new jose.SignJWT({ email:"***REMOVED***" }).setProtectedHeader({ alg }).setExpirationTime('1d').sign(secret);
     console.log(jwt)
 }
 
